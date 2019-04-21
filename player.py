12a82012db4bb06956fbda8494dd039df539a99b
Neyte <neya-liz@mail.ru>
2019-04-21 20:30:33 +0300

    update of update

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 import pygame
 from params import *
+from math import sqrt
 vec = pygame.math.Vector2
 
 class Player(pygame.sprite.Sprite):
@@ -33,14 +34,36 @@ class Player(pygame.sprite.Sprite):
         self.image = self.game.spritesheet.get_image(0, 0, 72, 95)
 
     def update(self):
-        self.acc = vec(0, GRAVITY_ACC)
+
+        '''
+        self.rect.bottom = 120
+        platform = pygame.sprite.spritecollide(self, self.game.platforms, False)
+        if platform:
+            print(1)
+
+        hit = self.is_on_ground()
+        if hit:
+            self.on_ground = True
+        if self.on_ground:
+            print(2)
+        return  #   '''
+
+
+
 
         # check ground
         hit = self.is_on_ground()
         if hit:
             self.on_ground = True
-            self.vel.y = 0
-            self.pos.y = hit[0].rect.top
+            # self.vel.y = 0
+            # self.pos.y = hit[0].rect.top + (hit[0].pos.y - hit[0].rect.centery)
+        else:
+            self.on_ground = False
+
+        if self.on_ground:
+            self.acc = vec(0, 0)
+        else:
+            self.acc = vec(0, GRAVITY_ACC)
 
         keys = pygame.key.get_pressed()
         if self.on_ground:
@@ -48,20 +71,43 @@ class Player(pygame.sprite.Sprite):
                 self.acc.x = -PLAYER_ACC
             if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
                 self.acc.x = PLAYER_ACC
-
-        # jump
-        if self.on_ground and keys[pygame.K_SPACE]:
-            self.jump()
+            # jump
+            if keys[pygame.K_SPACE] or keys[pygame.K_UP] or keys[pygame.K_d]:
+                self.jump()
 
         # apply friction
         if self.on_ground:
             self.acc.x += self.vel.x * PLAYER_FRICTION
 
+
         # equations of motion
         self.vel += self.acc
         if abs(self.vel.x) < 0.1:
             self.vel.x = 0
-        self.pos += self.vel + 0.5 * self.acc
+        old_pos = self.pos
+        new_pos = old_pos + self.vel + 0.5 * self.acc
+
+        vec_len = int(sqrt((old_pos.x - new_pos.x)**2 + (old_pos.y - new_pos.y)**2)) + 1
+        final_pos = vec(0, 0)
+        for i in range(vec_len):
+            self.pos = old_pos + (new_pos - old_pos) * (i + 1) / vec_len
+            # смещаем плеера для проверки новой позиции...
+            self.rect.centery += round(self.pos.y - old_pos.y)
+            self.rect.centerx += round(self.pos.x - old_pos.x)
+            platform = pygame.sprite.spritecollide(self, self.game.platforms, False)
+            for plat in platform:
+
+                if ((plat.rect.top <= self.rect.top <= plat.rect.bottom) or (plat.rect.top <= self.rect.bottom <= plat.rect.bottom)) and (self.rect.left <= plat.rect.left) and (plat.rect.right <= self.rect.right):
+                    self.vel.y = 0
+                if ((plat.rect.left <= self.rect.left <= plat.rect.right) or (plat.rect.left <= self.rect.right <= plat.rect.right)) and (self.rect.top <= plat.rect.top) and (plat.rect.bottom <= self.rect.bottom):
+                    self.vel.x = 0
+            # возвращаем плеера обратно...
+            self.rect.centery -= round(self.pos.y - old_pos.y)
+            self.rect.centerx -= round(self.pos.x - old_pos.x)
+            if platform:
+                self.pos = old_pos + (new_pos - old_pos) * i / vec_len
+                #pygame.quit();
+                break
 
         # wrap around the sides of the screen
         '''if self.pos.x > WIDTH:
@@ -76,7 +122,9 @@ class Player(pygame.sprite.Sprite):
             self.vel.y = 0
             self.pos.y = cur_plat[0].rect.top + (cur_plat[0].pos.y - cur_plat[0].rect.centery)'''
 
+
         screen_offset = vec(0, 0)
+
         if self.pos.x > WIDTH / 2:
             screen_offset.x = int(self.pos.x - WIDTH / 2)
         if self.pos.x > len(self.game.lmap.level_array[0])*20 - WIDTH / 2:
@@ -97,9 +145,9 @@ class Player(pygame.sprite.Sprite):
 
 
     def is_on_ground(self):
-        self.pos.y += self.vel.y + 0.5 * self.acc.y + 1
+        self.rect.centery += 1
         hit = pygame.sprite.spritecollide(self, self.game.platforms, False)
-        self.pos.y -= self.vel.y + 0.5 * self.acc.y + 1
+        self.rect.centery -= 1
         return hit
 
 
