51af0ad349fe94cf4ad1b420aec82b2faeb765ca
TehnoBronik <mishamur21@gmail.com>
2019-04-21 19:47:53 +0300

    S

--- level_map.py
+++ level_map.py
@@ -1,11 +1,12 @@
 import pygame
 import numpy as np
-from platform import Platform
+from platform import *
 
 class Level_map():
     """Level matrices"""
 
-    def __init__(self, file_name):
+    def __init__(self, file_name, game):
+        self.game = game
         mapar = []
         i = 0
         with open(file_name, 'r') as file:
@@ -20,7 +21,8 @@ class Level_map():
         platforms_group = pygame.sprite.Group()
         for x in range(len(self.level_array)):
             for y in range(len(self.level_array[0])):
-                if self.level_array[x, y] == 1:
-                    platforms_group.add(Platform(y * 20, x * 20, 20, 20, self))
+                platform_type = self.level_array[x, y]
+                if platform_type == 0:
+                    continue
+                platforms_group.add(Platform(y * 40, x * 40, 40, 40, self.game, platform_type))
         return platforms_group
-
